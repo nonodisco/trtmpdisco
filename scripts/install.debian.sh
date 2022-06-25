60cc6bb780b8c502094db4318d02b07a39209cf3
nonodisco <107286410+nonodisco@users.noreply.github.com>
2022-06-25 21:13:05 +0300

    Update install.debian.sh

--- scripts/install.debian.sh
+++ scripts/install.debian.sh
@@ -1,1 +1,1 @@
-ARCH=$(dpkg --print-architecture) && curl -s https://api.github.com/repos/callsmusic/trtmp/releases/latest | grep "linux-$ARCH" | cut -d : -f 2,3 | tr -d \" | wget -O "linux-$ARCH" -qi -
+ARCH=$(dpkg --print-architecture) && curl -s https://api.github.com/repos/nonodisco/trtmpdisco/releases/latest | grep "linux-$ARCH" | cut -d : -f 2,3 | tr -d \" | wget -O "linux-$ARCH" -qi -
